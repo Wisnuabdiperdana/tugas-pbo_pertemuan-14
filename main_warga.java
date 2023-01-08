75d610aeec7e10f7f12a546a23c5fb0e2d6bc6ca
Wisnuabdiperdana <114768451+Wisnuabdiperdana@users.noreply.github.com>
2023-01-08 09:34:29 +0700

    Create main_warga.java

---
+++ main_warga.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class main_warga  {
+    public static void main_warga (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer pilihan = 0;
+
+        System.out.println("====================================");
+        System.out.println("Pendataan dan Pembayaran Iuran Warga");
+        System.out.println("====================================");
+        System.out.println("1. Input Data Warga");
+        System.out.println("2. Input Iuran Warga");
+        System.out.println();
+        System.out.print("PILIHAN MENU : ");
+        pilihan = sc.nextInt();
+        System.out.println();
+
+        datawargabandung n = new datawargabandung();
+
+        if (pilihan == 1){
+            n.warga1();
+            //}else if(pilihan == 2){
+            n.warga2();
+            }
+        }
+    }
